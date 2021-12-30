f6a7122ba6f8298b1f7f01cf46181141d855eb44
ahamedashik01 <ashikshamim.007@gmail.com>
2021-12-30 14:27:30 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
+import LogIn from './pages/LogIn/LogIn';
+import Register from './pages/Register/Register';
+
+
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="/login">
+            <LogIn></LogIn>
+          </Route>
+          <Route path="/register">
+            <Register></Register>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
